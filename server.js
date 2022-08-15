9641d208f9d001aa9d4b7ee0e79d4688f6bddd69
David <45941940+Tubohaler@users.noreply.github.com>
2022-08-15 10:52:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,9 @@ const express = require("express");
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-const aws = require('aws-sdk');
-
-let s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET
-});
 
 app.get("/", (req, res) => {
-  res.send("Goodday World!");
+  res.send(process.env.DUMMY_TEXT);
 });
 
 app.listen(PORT, () => {
